24fdca0ae4644e8a34eb85c97c4b8c0a4d8c0270
ndnestor <trabajo.nestor.098@gmail.com>
2025-04-08 16:58:34 +0200

    Bug fixed

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
         e.preventDefault(); // Previene el comportamiento por defecto del formulario
         setIsSubmitting(true);
 
-        post('/login', {
+        post(import.meta.env.APP_URL + '/login', {
             onError : ()=>{
                 setIsSubmitting(false);
             },
