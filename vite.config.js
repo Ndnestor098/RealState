ebea6fd5343a01453bad6dfb4ed7f37b10f249ec
ndnestor <trabajo.nestor.098@gmail.com>
2025-04-09 11:29:38 +0200

    Bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,55 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        https: true,
-        host: 'villa.ndnestor.com',
-    },
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: 'resources/js/app.jsx',
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: `${env.VITE_URL}/build/`, // 👈 Esto es la clave
+        plugins: [
+            laravel({
+                input: [
+                    'resources/js/app.jsx',
+                    'resources/css/fontawesome.css',
+                    'resources/css/templatemo-villa-agency.css',
+                    'resources/css/animate.css',
+                    'resources/css/flex-slider.css',
+                    'resources/css/owl.css',
+                    'resources/css/style.css',
+                ],
+                ssr: 'resources/js/ssr.jsx',
+                refresh: false,
+            }),
+            react(),
+        ],
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+
+                        if (id.includes('resources/js/components/')) {
+                            const name = path.basename(id).replace('.jsx', '');
+                            return `components/${name}`;
+                        }
+                    }
+                }
+            },
+            manifest: true,
+            outDir: 'public/build',
         },
-    },
-    base: '/build/',
+        server: {
+            https: true,
+            host: 'villa.ndnestor.com',
+        },
+        resolve: {
+            alias: {
+                '@': '/resources/js',
+            },
+        },
+    };
 });
