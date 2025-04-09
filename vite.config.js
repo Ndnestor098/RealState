8adbe1b1c0ac037331a9ab14ece439a06dd67a01
ndnestor <trabajo.nestor.098@gmail.com>
2025-04-09 11:00:34 +0200

    Bug fixed

--- vite.config.js
+++ vite.config.js
@@ -3,50 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/build/',
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.jsx',
-                'resources/css/fontawesome.css',
-                'resources/css/templatemo-villa-agency.css',
-                'resources/css/animate.css',
-                'resources/css/flex-slider.css',
-                'resources/css/owl.css',
-                'resources/css/style.css',
-
-            ],
-            ssr: 'resources/js/ssr.jsx',
-            refresh: false,
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,
         }),
-        react(),
     ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return 'vendor'; // Mantener vendor separado
-                    }
-    
-                    // Divide los componentes grandes
-                    if (id.includes('resources/js/components/')) {
-                        const name = id.split('/').pop().replace('.jsx', '');
-                        return `components/${name}`; // Crea un chunk por cada componente
-                    }
-                }
-            }
-        }
-    },
     server: {
-        https: true,
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
+        https: true, 
+        host: 'villa.ndnestor.com',
     },
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-        },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
     },
+    base: '/build/',
 });
