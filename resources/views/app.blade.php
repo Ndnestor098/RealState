7752f11a38e034db7eb35dbedd94b0f6e0d6120f
ndnestor <trabajo.nestor.098@gmail.com>
2025-04-12 16:37:43 +0200

    change in the index blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -47,11 +47,6 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        {{-- <link rel="stylesheet" href="/css/fontawesome.css">
-        <link rel="stylesheet" href="/css/templatemo-villa-agency.css">
-        <link rel="stylesheet" href="/css/owl.css">
-        <link rel="stylesheet" href="/css/animate.css">
-        <link rel="stylesheet" href="/css/style.css"> --}}
         @vite(['resources/js/app.jsx'])
         @vite('resources/css/fontawesome.css')
         @vite('resources/css/templatemo-villa-agency.css')
